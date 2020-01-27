cb78ad8fafca548029754d5925ad67741b3b6df4
Your Name <you@example.com>
2020-01-27 16:56:13 -0300

    .

---
+++ Providers/CompanyOrDesignServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\CompanyOrDesign\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class CompanyOrDesignServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('CompanyOrDesign', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('CompanyOrDesign', 'Config/config.php') => config_path('companyordesign.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('CompanyOrDesign', 'Config/config.php'), 'companyordesign'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/companyordesign');
+
+        $sourcePath = module_path('CompanyOrDesign', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/companyordesign';
+        }, \Config::get('view.paths')), [$sourcePath]), 'companyordesign');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/companyordesign');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'companyordesign');
+        } else {
+            $this->loadTranslationsFrom(module_path('CompanyOrDesign', 'Resources/lang'), 'companyordesign');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('CompanyOrDesign', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
